7b146b435cfca3087ba1ecc66fc16f0053df1e26
Etherealite <evanbanghm@yahoo.com>
2012-02-16 08:14:21 -0800

    started testloop.py to build twilio callbacks off of before the service functionality is complete

--- testloop.py
+++ testloop.py
@@ -1,3 +1,14 @@
+from threading import Thread
+import time
+import webface
+
+
+
+kw_args = {'host':'localhost', 'port':8080}
+t = Thread(target=webface.run, kwargs=kw_args)
+t.daemon = True
+t.start()
+
 class Problem:
   pass
 
@@ -7,8 +18,6 @@ problem2 = Problem()
 problem2.msg = " there is fuckity derp problem"
 problems = [problem1, problem2]
 
-def sendcall():
-  pass
 
 
 while True:
@@ -22,5 +31,13 @@ while True:
       else:
         first_msg = False
         msg += problem.msg
+    webface.problems = len(problems)
+    webface.message = msg
     problems.pop()
+    print msg
+  else:
+    webface.problems = 0
+    webface.message = "no problems reported"
+    del t
+  time.sleep(3)
 
--- webface.py
+++ webface.py
@@ -1,14 +1,9 @@
 from bottle import route, run
-from multiprocessing import Process
 import time
-
+problems = 0
+message = ""
 @route('/')
 def index():
-    return 'service check web interface'
+    return 'problems {} <br> {}'.format(problems, message)
 
-kw_args = {'host':'localhost', 'port':8080}
-p = Process(target=run, kwargs=kw_args)
-p.start()
-#p.join()
-#run(host='localhost', port=8080)
 
